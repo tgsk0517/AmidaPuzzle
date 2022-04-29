76caf12005cd1f283e89f763301c654776c9e9c5
tgsk0517 <opnj0517@gmail.com>
2022-04-29 16:16:56 +0900

    画面移動処理

--- Assets/Users/Togasaki/Script/PuzzleManager.cs
+++ Assets/Users/Togasaki/Script/PuzzleManager.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
+    [SerializeField, Header("パズルオブジェクト")]
+    private GameObject puzzleObj;
+
     ///パズルのパターン数
     public static int MaxPuzzlePattern = 6;
+
+    /// <summary>
+    /// パズルパターンの乱数
+    /// </summary>
+    private int PuzzlePatternNum()
+    {
+        int num = Random.Range(0, MaxPuzzlePattern);
+        return num;
+    }
+
+    private void GeneratePuzzle()
+    {
+
+    }
 
 }
---
+++ Assets/Users/Togasaki/Script/PuzzleTransformer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTransformer : MonoBehaviour
+{
+    [SerializeField,Header("パズルオブジェクト群(左上から右へ1つずつ進んでいき、0～8に格納)")]
+    private GameObject[] puzzleObj;
+
+
+}
